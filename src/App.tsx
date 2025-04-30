9347a5380039859ad67118163adfaf360d94c697
John Pope <jp+mailopoly@bellgeorge.com>
2025-04-30 21:28:28 +1000

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ const AppContent: React.FC = () => {
           <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
             <Routes>
               <Route index path="/" element={<HomePage />} />
-              <Route path="/knowledge" element={<KnowledgeBase />} />
+              <Route path="/auth" element={<KnowledgeBase />} />
               <Route path="/questions" element={<InterviewQuestions />} />
               <Route path="/chat" element={<ChatPage />} />
               <Route path="/settings" element={<Settings />} />
